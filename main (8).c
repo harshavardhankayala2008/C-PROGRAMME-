aa6e1ddf66d6fb61143ba09cb62552ddbca24793
harshavardhankayala2008 <harshavardhankayala2008@gmail.com>
2026-07-23 21:43:18 +0530

    Add files via upload

---
+++ main (8).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+   int password;
+   printf("enter the correct password");
+   scanf("%d",&password);
+   if(1234)
+   {
+       printf("correct password");
+   }
+   else
+   {
+       printf("incorrect password");
+   }
+   return 0;
+}
