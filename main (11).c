d2d318b1f193534d9cf0779321225aa16e462f45
harshavardhankayala2008 <harshavardhankayala2008@gmail.com>
2026-07-24 10:44:33 +0530

    Add files via upload

---
+++ main (11).c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    float f;
+    char c;
+    double d;
+    printf("\nthe size of n is %d bytes",sizeof(n));
+    printf("\nthe size of f is %d bytes",sizeof(f));
+    printf("\nthe size of c is %d bytes",sizeof(c));
+    printf("\nthe size of d is %d bytes",sizeof(d));
+
+    return 0;
+}
