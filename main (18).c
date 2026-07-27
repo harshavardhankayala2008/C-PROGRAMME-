b25c1ff6b49c2acac37d2575e81fedc5f508c169
harshavardhankayala2008 <harshavardhankayala2008@gmail.com>
2026-07-27 21:30:24 +0530

    Add files via upload

---
+++ main (18).c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c,alpha,beeta,dis;
+    printf("enter the co-efficients :");
+    scanf("%d%d%d",&a,&b,&c);
+    dis=(b*b-4*a*c);
+    if(dis>=0)
+        {
+            alpha=(-b+sqrt(dis))/2*a;
+            beeta=(-b-sqrt(dis))/2*a;
+            printf("the roots are \nalpha=%d\nbeeta=%d",alpha,beeta);
+        }
+    else
+    {
+        printf("the roots are imaginary");
+    }
+ return 0;
+}
