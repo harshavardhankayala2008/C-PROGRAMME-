49bb07020fbf9d72c3265816c4ff4aa3664b68d5
harshavardhankayala2008 <harshavardhankayala2008@gmail.com>
2026-07-29 20:58:45 +0530

    Add files via upload

---
+++ main (36).c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    char op;
+    printf("simple calculator:");
+    scanf("%d%d",&a,&b);
+    printf("enter the operator");
+    scanf(" %c",&op);
+    switch(op)
+    {
+        case '+':
+        printf("\n%d+%d=%d",a,ba+b);
+        break;
+        case '-':
+        printf("\n%d-%d=%d",a,b,a-b);
+        break;
+        case '*':
+        printf("\n%d*%d=%d",a,b,a*b);
+        break;
+        case '/':
+        if(b!=0)
+        {
+           printf("%d/%d=%d",a,b,a/b); 
+        }
+        else
+        {
+            printf("it is not possible:");
+        }
+        
+        
+        break;
+        default:
+        printf("the operator is invalid");
+    }
+    return 0;
+}
