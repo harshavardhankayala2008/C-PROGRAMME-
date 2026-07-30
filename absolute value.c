07e02709c1824ce1c67927c66b361b4d9aa83c63
harshavardhankayala2008 <harshavardhankayala2008@gmail.com>
2026-07-30 10:03:51 +0530

    Update and rename main (10).c to absolute value.c

---
+++ absolute value.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter any one number:");
+    scanf("%d",&n);
+    if(n>=0)
+    {
+        printf("the given number is positive:");
+    }
+    else
+    {
+        printf("the given number is negitive:");
+    }
+
+    return 0;
+}
