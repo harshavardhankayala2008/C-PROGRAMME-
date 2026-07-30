1ac36448fbe218a9f310f5080c2c7b471bbe0f49
harshavardhankayala2008 <harshavardhankayala2008@gmail.com>
2026-07-30 10:04:52 +0530

    Update and rename main (11).c to size of data type.c

---
+++ size of data type.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    float f;
+    char c;
+    double d;
+    printf("\nthe size of n is %d bytes",sizeof(n));
+    printf("\nthe size of f is %d bytes",sizeof(f));
+    printf("\nthe size of c is %d bytes",sizeof(c));
+    printf("\nthe size of d is %d bytes",sizeof(d));
+
+    return 0;
+}
