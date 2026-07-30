edfe1a6526436a7609f926f958a50fa3e94141d7
harshavardhankayala2008 <harshavardhankayala2008@gmail.com>
2026-07-30 10:29:08 +0530

    Update and rename main (32).c to factorial.c

--- factorial.c
+++ factorial.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int n,fact=1;
+    int n,fact=1,temp;
     printf("enter any one number:");
     scanf("%d",&n);
+    temp=n;
     while(n>=1)
     {
         fact=fact*n;
         n--;
     }
-    printf("the factorial of %d is %d",n,fact);
+    printf("the factorial of %d is %d",temp,fact);
     return 0;
 }
