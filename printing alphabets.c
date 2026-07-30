544d8ee6591a65da43628baf27c0e2b425123663
harshavardhankayala2008 <harshavardhankayala2008@gmail.com>
2026-07-30 21:53:38 +0530

    Create printing alphabets.c

---
+++ printing alphabets.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int r,c;
+    for(r=65;r<=69;r++)
+    {
+        for(c=65;c<=69;c++)
+        printf(" %c ",c);
+        printf("\n");
+    }
+    return 0;
+}
