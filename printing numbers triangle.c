540c4cd481efff62a46d61a291936ad286c2e878
harshavardhankayala2008 <harshavardhankayala2008@gmail.com>
2026-08-05 20:34:29 +0530

    Add program to print number triangle pattern

---
+++ printing numbers triangle.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int r,lc,rc,sp,i;
+    for(r=1;r<=5;r++)
+    {
+        for(sp=1;sp<=40-r;sp++)
+        {
+            printf(" ");
+        }
+        for(lc=1;lc<=r;lc++)
+        {
+            printf("%d",lc);
+        }
+        for(rc=r-1;rc>=1;rc--)
+        {
+            printf("%d",rc);
+        }
+        printf("\n");
+    }
+    return 0;
+}
