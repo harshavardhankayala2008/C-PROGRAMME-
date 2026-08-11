ac521bf3ea0bb4db0aaf4818f8ef6e97c7644cf6
harshavardhankayala2008 <harshavardhankayala2008@gmail.com>
2026-08-11 10:12:17 +0530

    Create tsa of hemi sphere.c

---
+++ tsa of hemi sphere.c
@@ -0,0 +1,12 @@
+/*tsa of hemisphere*/
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float pi=3.141,r,tsa;
+    printf("enter the value of r:");
+    scanf("%f",r);
+    tsa=3*pi*r*r;
+    printf("the tsa of hemi sphere is %f",tsa);
+    return 0;
+}
