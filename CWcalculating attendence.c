b2c619e6873ddc9f3d8b83cdf8c3fb6d793b89a0
harshavardhankayala2008 <harshavardhankayala2008@gmail.com>
2026-08-12 18:53:30 +0530

    Create CWcalculating attendence.c

---
+++ CWcalculating attendence.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float cc,ac,aap;
+    printf("enter cc&ac");
+    scanf("%f%f",&cc,&ac);
+    aap=((float)ac/cc)*100;
+    printf("aap=%f",aap);
+    return 0;
+}
