fbb410fe110d10c1c38f526f92feaeebe8d17e9d
harshavardhankayala2008 <harshavardhankayala2008@gmail.com>
2026-08-14 14:00:01 +0530

    Create conversion from uppercase to lowercase.c

---
+++ conversion from uppercase to lowercase.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char c1;
+    printf("enter uppercase letters");
+    scanf("%c",&c1);
+    c1=c1+32;
+    printf("enter the lower case=%c",c1);
+    return 0;
+}
