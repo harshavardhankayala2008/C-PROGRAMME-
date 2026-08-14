673a9548e18dc47827391c10b8a10fcbced49ac6
harshavardhankayala2008 <harshavardhankayala2008@gmail.com>
2026-08-14 14:06:43 +0530

    Create combination of digits.c

---
+++ combination of digits.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,n;
+    printf("enter the numbers a,b,c");
+    scanf("%d%d%d",&a,&b,&c);
+    n=(a*100)+(b*10)+(c*1);
+    printf("the number is %d",n);
+    return 0;
+}
