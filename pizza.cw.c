01ea28d0e96e340a1fad639a16fc5bd3a1aa1750
harshavardhankayala2008 <harshavardhankayala2008@gmail.com>
2026-08-20 15:40:26 +0530

    Add pizza slice calculator in pizza.cw.c

---
+++ pizza.cw.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int pizza,slices;
+    printf("enter the no.of pizzas");
+    scanf("%d",&pizza);
+    slices=8*pizza;
+    printf("the no of pizzas=%d\nthe number of slices %d",pizza,slices);
+    return 0;
+}
